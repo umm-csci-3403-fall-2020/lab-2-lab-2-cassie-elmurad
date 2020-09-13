894de5b16994d26f3761e00084548ab951fb8e48
Cassie Schultz <schu4276@morris.umn.edu>
2020-09-13 16:34:14 -0500

    I got a start on the disemvowel.c.  I have the general skeleton of a while loop, and need to figure out how to make sure the length of the new string is exavtly the length of the string without vowels.

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -3,5 +3,16 @@
 #include "disemvowel.h"
 
 char *disemvowel(char *str) {
+  char *newString;
+  int i = 0;
+  while(str[i] != '\0'){
+     if(str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' ){
+        i++; 
+     } else{
+        
+	     
+     }
+
+  }// end while 
   return (char*) "";
 }
