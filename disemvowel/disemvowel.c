78122e4a1b77e55f66c961a57e46acbf79f338d0
Cassie Schultz <schu4276@morris.umn.edu>
2020-09-15 11:10:01 -0500

    I have created a disemvowel that passes all but two tests as of now.  I made one function that checks for vowels in the given string, and another that uses that information to create the new string using a while loop.

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -1,18 +1,40 @@
 #include <stdlib.h>
-
+#include<cstring>
+#include<ctype.h>
 #include "disemvowel.h"
 
+int vowelCheck(char *str){
+char letter;
+int i =0;
+  int stringLength = strlen(str);
+  letter = tolower(str[i]);
+  if(letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u' ){
+        stringLength--;
+  }
+      return stringLength;
+}
+
+
 char *disemvowel(char *str) {
-  char *newString;
-  int i = 0;
+  char *newString, oldletter;
+  int i =0, j=0, newStringLength=0;
+  // string length gets plus one because of null terminator
+  newStringLength = vowelCheck(str);
+  newString = (char*) calloc(newStringLength+1, sizeof(char));
   while(str[i] != '\0'){
-     if(str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' ){
-        i++; 
-     } else{
-        
+	oldletter = str[i];
+     if(oldletter == 'a' || oldletter == 'e' || oldletter == 'i' || oldletter == 'o' || oldletter == 'u' ){	
+	i++;
+     } else{ 
+	newString[j] = str[i];
+ 	j++;
+	i++;
 	     
      }
-
-  }// end while 
-  return (char*) "";
+     // Add null thing to end 
+     newString[newStringLength+1] = '\0';   
+  }
+  return newString;
 }
+
+
